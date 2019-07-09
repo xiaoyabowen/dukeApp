f1f0f80b43f7d0eadbb69c70f826c4d7aef6fd18
wenqiangY <565435319@qq.com>
2019-07-09 19:21:54 +0800

    2019.7.9 19:21

---
+++ widget/html/user/chat/comment/contacts.js
@@ -0,0 +1,22 @@
+
+function contactsInit(Vue) {
+    var str = dataValue('user/chat/comment/contacts.html')
+
+    return {
+        template: str,
+        data: function() {
+            return {
+
+            }
+        },
+        created: function() {
+
+        },
+        mounted :function (){
+
+        },
+        methods: {
+
+        }
+    }
+}
---
+++ widget/html/user/chat/comment/resume.js
@@ -0,0 +1,22 @@
+
+function resumeInit(Vue) {
+    var str = dataValue('user/chat/comment/resume.html')
+
+    return {
+        template: str,
+        data: function() {
+            return {
+
+            }
+        },
+        created: function() {
+
+        },
+        mounted :function (){
+
+        },
+        methods: {
+
+        }
+    }
+}
