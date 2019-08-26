0f6b66bca168b698ddce4198cb2469080d2063bf
bowenxiaoya <2317446934@qq.com>
2019-08-26 18:58:03 +0800

    zz

--- widget/script/public.js
+++ widget/script/public.js
@@ -5,18 +5,18 @@ var isCleanUser = false;
 // var serverUrl = "http://192.168.1.22:8000/SE4M/SE/";
 
 
-/*
 // 本地服务器
 var serverUrl = "http://192.168.1.12:8000/SE4M/SE";
 var localhostHref = 'http://192.168.1.12:8000/SE4M';
 var Domain = 'http://192.168.1.12:8088';
-*/
 
 
+/*
 // 正式服务器
 var serverUrl = "http://112.126.98.172:8000/SE4M/SE";
 var localhostHref = 'http://112.126.98.172:8000/SE4M';
 var Domain = 'http://112.126.98.172:8088';
+*/
 
 
 var rootWindowName = "root";
