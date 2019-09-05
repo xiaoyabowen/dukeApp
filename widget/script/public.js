767913f0757e0707b23b04a61593e7304e06de68
bowenxiaoya <2317446934@qq.com>
2019-09-05 21:14:32 +0800

    0905 wan

--- widget/script/public.js
+++ widget/script/public.js
@@ -13,12 +13,12 @@ var localhostHref = 'http://192.168.1.24:8000/SE4M';
 var Domain = 'http://192.168.1.24:8088';
 
 
-
-
 // 正式服务器
-// var serverUrl = "http://112.126.98.172:8000/SE4M/SE";
-// var localhostHref = 'http://112.126.98.172:8000/SE4M';
-// var Domain = 'http://112.126.98.172:8088';
+/*
+var serverUrl = "http://112.126.98.172:8000/SE4M/SE";
+var localhostHref = 'http://112.126.98.172:8000/SE4M';
+var Domain = 'http://112.126.98.172:8088';
+*/
 
 
 
