9260feb5ad43643824f91f759d00051a5b5247f6
Mstian <1404746239@qq.com>
2019-09-07 18:42:56 +0800

    09071842

--- widget/script/public.js
+++ widget/script/public.js
@@ -14,7 +14,6 @@ var Domain = 'http://192.168.1.24:8088';
 
 
 // 正式服务器
-
 // var serverUrl = "http://112.126.98.172:8000/SE4M/SE";
 // var localhostHref = 'http://112.126.98.172:8000/SE4M';
 // var Domain = 'http://112.126.98.172:8088';
@@ -32,12 +31,11 @@ var Domain = 'http://112.126.98.172:8088';
 
 
 // 渡客正式服务器
-/*
-var serverUrl = "http://59.110.231.40:25121/SE4M/SE";
-var localhostHref = 'http://59.110.231.40:25121/SE4M';
-var Domain = 'http://59.110.231.40:25122';
 
-*/
+// var serverUrl = "http://59.110.231.40:25121/SE4M/SE";
+// var localhostHref = 'http://59.110.231.40:25121/SE4M';
+// var Domain = 'http://59.110.231.40:25122';
+
 
 
 
@@ -820,11 +818,17 @@ function conver(limit){
 
 // 图片路劲去逗号
 function imgSrcFun(src) {
-    /*if (isStr(src)) {
-        console.error('src is\'t string');
-        return false
-    }*/
-    return src.split(',')[0];
+    // if (isStr(src)) {
+    //     console.error('src is\'t string');
+    //     return false
+    // }
+    console.log(src,'825555')
+    if(src.indexOf(',')>-1){
+        return src.split(',')[0];
+    }else{
+      return src
+    }
+
 }
 
 // 字符串 转数组
@@ -1949,6 +1953,3 @@ function posiShareClick() {
             }*/
     });
 }
-
-
-
