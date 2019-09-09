af60b73365aa700212f12daf47409b77b4bdbe1f
Mstian <1404746239@qq.com>
2019-09-09 18:58:21 +0800

    09091858

--- widget/script/public.js
+++ widget/script/public.js
@@ -8,15 +8,15 @@ var isCleanUser = false;
 
 
 // 本地服务器
-var serverUrl = "http://192.168.1.24:8000/SE4M/SE";
-var localhostHref = 'http://192.168.1.24:8000/SE4M';
-var Domain = 'http://192.168.1.24:8088';
+// var serverUrl = "http://192.168.1.24:8000/SE4M/SE";
+// var localhostHref = 'http://192.168.1.24:8000/SE4M';
+// var Domain = 'http://192.168.1.24:8088';
 
 
 // 正式服务器
-// var serverUrl = "http://112.126.98.172:8000/SE4M/SE";
-// var localhostHref = 'http://112.126.98.172:8000/SE4M';
-// var Domain = 'http://112.126.98.172:8088';
+var serverUrl = "http://112.126.98.172:8000/SE4M/SE";
+var localhostHref = 'http://112.126.98.172:8000/SE4M';
+var Domain = 'http://112.126.98.172:8088';
 
 
 
@@ -33,7 +33,7 @@ var Domain = 'http://112.126.98.172:8088';*/
 // var serverUrl = "http://59.110.231.40:25121/SE4M/SE";
 // var localhostHref = 'http://59.110.231.40:25121/SE4M';
 // var Domain = 'http://59.110.231.40:25122';
-
+//
 
 
 
