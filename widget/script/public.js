0ace2fa67b080aaea5114fb92251c8d3418d3c92
Mstian <1404746239@qq.com>
2019-09-16 16:27:01 +0800

    09161626

--- widget/script/public.js
+++ widget/script/public.js
@@ -9,16 +9,16 @@ var isCleanUser = false;
 
 // 本地服务器
 
-// var serverUrl = "http://192.168.1.24:8000/SE4M/SE";
-// var localhostHref = 'http://192.168.1.24:8000/SE4M';
-// var Domain = 'http://192.168.1.24:8088';
+var serverUrl = "http://192.168.1.24:8000/SE4M/SE";
+var localhostHref = 'http://192.168.1.24:8000/SE4M';
+var Domain = 'http://192.168.1.24:8088';
 
 
 
 // 正式服务器
-var serverUrl = "http://112.126.98.172:8000/SE4M/SE";
-var localhostHref = 'http://112.126.98.172:8000/SE4M';
-var Domain = 'http://112.126.98.172:8088';
+// var serverUrl = "http://112.126.98.172:8000/SE4M/SE";
+// var localhostHref = 'http://112.126.98.172:8000/SE4M';
+// var Domain = 'http://112.126.98.172:8088';
 
 
 
