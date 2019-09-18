7c5ea78ae4ddcc1dc9a66c8b1dfcdc52c9371fdf
Mstian <1404746239@qq.com>
2019-09-18 19:53:00 +0800

    09181952

--- widget/script/public.js
+++ widget/script/public.js
@@ -9,14 +9,14 @@ var isCleanUser = false;
 // 本地服务器
 
 
-// var serverUrl = "http://192.168.1.6:8000/SE4M/SE";
-// var localhostHref = 'http://192.168.1.6:8000/SE4M';
-// var Domain = 'http://192.168.1.6:8088';
-//
+var serverUrl = "http://192.168.1.6:8000/SE4M/SE";
+var localhostHref = 'http://192.168.1.6:8000/SE4M';
+var Domain = 'http://192.168.1.6:8088';
 
 
 
-// // 正式服务器
+
+// 正式服务器
 // var serverUrl = "http://112.126.98.172:8000/SE4M/SE";
 // var localhostHref = 'http://112.126.98.172:8000/SE4M';
 // var Domain = 'http://112.126.98.172:8088';
@@ -24,10 +24,10 @@ var isCleanUser = false;
 
 // 渡客正式服务器
 
-var serverUrl = "http://59.110.231.40:25121/SE4M/SE";
-var localhostHref = 'http://59.110.231.40:25121/SE4M';
-var Domain = 'http://59.110.231.40:25122';
-
+// var serverUrl = "http://59.110.231.40:25121/SE4M/SE";
+// var localhostHref = 'http://59.110.231.40:25121/SE4M';
+// var Domain = 'http://59.110.231.40:25122';
+//
 
 
 var rootWindowName = "root";
@@ -208,6 +208,9 @@ var DeleteEducation = serverUrl + "/UserProfile/removeEducation";
 
 // 查询工作经历
 var GetWork = serverUrl + "/UserProfile/queryWorkingExperienceById";
+
+var getWorkById = serverUrl + '/WorkingExperienceProfile/queryWorkingExperienceSummary';
+
 // 添加工作经历
 var AddWork = serverUrl + "/WorkingExperienceProfile/addWorkingExperience";
 // 删除工作经历
